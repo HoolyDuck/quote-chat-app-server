1140fa8cfdd3601e4bdd03b3c76e4b4dcecbfabe
HoolyDuck <nazar260804@gmail.com>
2024-08-08 18:16:56 +0300

    + user registration/retrieval on google login

--- src/lib/passport-oauth/passport-oauth.ts
+++ src/lib/passport-oauth/passport-oauth.ts
@@ -1,5 +1,12 @@
 import { Strategy } from "passport-google-oauth20";
 import { ENV_VARS } from "../../common/constants/env-vars";
+import { userService } from "../../services";
+
+type Payload = {
+  sub: string;
+  name: string;
+  picture: string;
+};
 
 const googleStrategy = new Strategy(
   {
@@ -7,8 +14,20 @@ const googleStrategy = new Strategy(
     clientSecret: ENV_VARS.GOOGLE_CLIENT_SECRET!,
     callbackURL: `${ENV_VARS.API_URL}/auth/google/callback`,
   },
-  async (accessToken, refreshToken, profile, done) => {
-    done(null, profile);
+  async (_accessToken, _refreshToken, profile, done) => {
+    const { sub, name, picture } = <Payload>profile._json;
+
+    const dbUser = await userService.getUserByGoogleId(sub);
+    if (!dbUser) {
+      const newUser = await userService.createUser({
+        googleId: sub,
+        name,
+        avatar: picture,
+      });
+      return done(null, newUser);
+    }
+
+    return done(null, dbUser);
   }
 );
 
