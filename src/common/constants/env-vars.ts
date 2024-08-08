fdc5160e439b68ec8415b9ce75e27071e3e7be83
HoolyDuck <nazar260804@gmail.com>
2024-08-08 03:31:21 +0300

    + setup env vars

---
+++ src/common/constants/env-vars.ts
@@ -0,0 +1,7 @@
+export const ENV_VARS = {
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  API_URL: process.env.API_URL,
+  PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
+};
